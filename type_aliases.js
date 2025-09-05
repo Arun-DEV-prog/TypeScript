b2c45f3e006dac5528abed8515a53d1b17ae3fec
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2025-09-05 21:34:10 +0600

    type aliases

--- type_aliases.js
+++ type_aliases.js
@@ -22,3 +22,15 @@ function padLeft(padding, input) {
     return padding + input;
 }
 console.log(padLeft('abul', 'arun'));
+function printAll(strs) {
+    if (strs && typeof strs == "object") {
+        for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
+            var s = strs_1[_i];
+            console.log(s);
+        }
+    }
+    else if (typeof strs === 'string') {
+        console.log(strs);
+    }
+}
+printAll(['arun', 'samad']);
--- type_aliases.ts
+++ type_aliases.ts
@@ -44,3 +44,16 @@ function padLeft(padding: number | string, input: string): string{
 }
 
 console.log(padLeft('abul','arun'));
+
+
+function printAll(strs: string | string [] | null){
+     if(strs && typeof strs == "object"){
+         for( const s of strs){
+             console.log(s)
+         }
+     }else if(typeof strs === 'string'){
+        console.log(strs);
+     }
+}
+
+printAll([]);
